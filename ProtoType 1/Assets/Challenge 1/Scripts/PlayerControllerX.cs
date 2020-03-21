5259c85419787c0355d839b8af603d2d0d73b3da
Adrian Thomas <thomasa8992@hotmail.com>
2020-03-21 14:17:41 -0500

    Added logic to control the tilt of the plane by player vertical axis

--- ProtoType 1/Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ ProtoType 1/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class PlayerControllerX : MonoBehaviour
 {
-    public float speed;
     public float rotationSpeed;
     public float verticalInput;
+    public float horizontalInput;
 
     // Start is called before the first frame update
     void Start()
@@ -15,15 +15,19 @@ public class PlayerControllerX : MonoBehaviour
     }
 
     // Update is called once per frame
-    void FixedUpdate()
-    {
-        // get the user's vertical input
-        verticalInput = Input.GetAxis("Vertical");
+    void FixedUpdate() {
+        var speed = 5.0f;
 
         // move the plane forward at a constant rate
-        transform.Translate(Vector3.forward * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+        handleTiltOfPlaneByPlayedInput();
+    }
+
+    private void handleTiltOfPlaneByPlayedInput() {
+        verticalInput = Input.GetAxis("Vertical");
 
         // tilt the plane up/down based on up/down arrow keys
-        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.right,  Time.deltaTime * rotationSpeed * verticalInput);
     }
 }
