acdd2c29569323b39375c5164099d85699c33230
Adrian Thomas <thomasa8992@hotmail.com>
2020-03-22 18:26:55 -0500

    added script that detects when an object is off the screena nd destroys that object

---
+++ Prototype 2/Assets/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+    public float topBoundary = 30;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        handleOutOfBoundsObject();
+    }
+
+    private void handleOutOfBoundsObject() {
+        if (transform.position.z > topBoundary) {
+            Destroy(gameObject);
+        }
+    }
+}   
