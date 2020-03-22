a2742ce48a39bef9f783bf6cb504f09905167ab1
Adrian Thomas <thomasa8992@hotmail.com>
2020-03-22 17:25:06 -0500

    Added script that moves a projectile forward

---
+++ Prototype 2/Assets/Scripts/MoveFoodForward.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveFoodForward : MonoBehaviour
+{
+    public float movementSpeed = 40.0f;
+    public float rotateAngle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotateAngle = 1;
+
+        transform.Translate(Vector3.forward * Time.deltaTime * movementSpeed);
+    }   
+}
