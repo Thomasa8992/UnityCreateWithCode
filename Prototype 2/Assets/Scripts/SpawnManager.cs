6b1466f974930f4458ffc63d68db97febb6a520d
Adrian Thomas <thomasa8992@hotmail.com>
2020-03-22 23:35:41 -0500

    Changed logic to spawn animals witout the user having to activate a keypress

--- Prototype 2/Assets/Scripts/SpawnManager.cs
+++ Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -7,21 +7,24 @@ public class SpawnManager : MonoBehaviour
     public GameObject[] animalPrefabs;
     private float spawnRangeX = 20;
     private float spawnPositionZ = 20;
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
+
+    }
+
+    private void SpawnRandomAnimal() {
         var animalPrefabIndex = Random.Range(0, animalPrefabs.Length);
         var spawnPosition = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPositionZ);
 
-        if (Input.GetKeyDown(KeyCode.S)) {
-            Instantiate(animalPrefabs[animalPrefabIndex], spawnPosition, animalPrefabs[animalPrefabIndex].transform.rotation);
-        }
+        Instantiate(animalPrefabs[animalPrefabIndex], spawnPosition, animalPrefabs[animalPrefabIndex].transform.rotation);
     }
 }
